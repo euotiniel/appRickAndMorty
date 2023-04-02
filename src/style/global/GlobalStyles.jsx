d6081c32eb099c34285d21132e4719d561c12664
Otoniel Emanuel <otonielemanuel21@gmail.com>
2023-04-02 17:40:28 +0100

    first commit

--- src/style/global/GlobalStyles.jsx
+++ src/style/global/GlobalStyles.jsx
@@ -1,6 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle `
+    ::-webkit-scrollbar {
+        width: 8px;
+        background-color: #000;
+    }
+  
+    ::-webkit-scrollbar-thumb {
+        background-color: #5fc0386c;
+        border-radius: 5px; 
+    }
+
     * {
         margin: 0;
         padding: 0;
